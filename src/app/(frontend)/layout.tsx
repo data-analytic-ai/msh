f2cbb7416d9f638ba8025f3543b9bdb0086445f2
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2025-03-18 11:35:31 -0600

    feat: Añadir banner de emergencia en el layout principal
    Cambios realizados:
    - Se ha creado un nuevo componente EmergencyBanner para mostrar un mensaje de emergencia.
    - Se ha integrado el EmergencyBanner en el layout principal de la aplicación.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -12,6 +12,7 @@ import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
+import { EmergencyBanner } from '@/components/emergency-ui/EmergencyBanner'
 
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -35,6 +36,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           />
 
           <Header />
+          <EmergencyBanner />
           {children}
           <Footer />
         </Providers>
